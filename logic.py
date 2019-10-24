80ba5942a40f9255d9147e8d0050badcd9b7ef67
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-10-24 13:31:04 -0400

    up to +250, made a bunch of functions smarter

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-from typing import Dict, Any, Tuple, List
+from typing import Dict, Any, Tuple, List, Optional
 from time import sleep
 from random import choice, sample
 
@@ -17,23 +17,38 @@ Session = sessionmaker()
 Base.metadata.create_all(engine, checkfirst=True)
 
 
-def check_if_db() -> Tuple[int, List[str]]:
+def check_if_db(room_id: Optional[int] = None, return_exits: bool = False) -> Tuple[int, List[str]]:
     """ checks if the current room you're in is in the database.
     if not, adds it.
     to be helpful, returns current room id
+
+    if given an int, it will check that room. not not, it will initialize.
     """
     sess = Session(bind=engine)
-    i = init()
-    sleep(i['cooldown'])
-    room_ = sess.query(Room).filter(Room.room_id==i['room_id'])
+    if not room_id:
+        i = init()
+        sleep(i['cooldown'])
+        room_id = i['room_id']
+
+    room_ = sess.query(Room).filter(Room.room_id==room_id)
 
     if not room_.all():
-        room = Room(room_id=i['room_id'], title=i['title'])
+        i = init()
+        sleep(i['cooldown'])
+        room = Room(room_id=room_id, title=i['title'])
         sess.add(room)
+        sess.commit()
 
-    sess.commit()
-    return i['room_id'], i['exits']
-  
+    if return_exits:
+        try:
+            return room_id, i['exits']
+        except NameError as e:
+            i = init()
+            sleep(i['cooldown'])
+            return room_id, i['exits']
+    else:
+        return room_id, []
+   
 
 def move(direction: str) -> Dict[str, Any]:
     """ attempting to move with wisdom """
@@ -50,7 +65,7 @@ def move(direction: str) -> Dict[str, Any]:
         m = move_(direction)
     sleep(m['cooldown'])
 
-    curr, _ = check_if_db()
+    curr, _ = check_if_db(m['room_id'])
     assert curr==m['room_id']
 
     # pardon this awful switch case business, prev_room.__dict__['direction_to'] didn't work.
@@ -82,7 +97,7 @@ def move(direction: str) -> Dict[str, Any]:
 
 def wander() -> Dict[str, Any]:
     sess = Session(bind=engine)
-    curr, exits = check_if_db()
+    curr, exits = check_if_db(return_exits=True)
     # check which exits are unexplored.
     curr_room = sess.query(Room).filter(Room.room_id==curr).all()[0]
     for direction in sample(exits, len(exits)):
@@ -97,10 +112,18 @@ def wander() -> Dict[str, Any]:
 def goto(start: int, end: int) -> Dict[str, Any]:
     sess = Session(bind=engine)
     graph = make_graph(sess)
-    paths = bf_paths(graph, start)
-    # print(paths)
-    path = paths[(start, end)]
-    print(f"it will take roughly under {11 * len(path) / 60} minutes to make {len(path)} moves")
+    try:
+        paths = bf_paths(graph, start)
+        path = paths[(start, end)]
+    except KeyError as e:
+        try:
+            paths = df_paths(graph, start)
+            path = paths[(start, end)]
+        except KeyError as e:
+            print("search for path failed. ")
+            return "search for path failed"
+           
+    print(f"it will take roughly under {9 * len(path) / 60} minutes to make {len(path)} moves")
     for direction in path:
         moved_to = move(direction)
         print(f"{moved_to['room_id']}: {moved_to['title']}")
